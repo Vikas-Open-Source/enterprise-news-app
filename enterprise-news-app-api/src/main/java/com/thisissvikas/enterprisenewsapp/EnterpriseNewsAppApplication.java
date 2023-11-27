8531e474fe0b1ab4721cdad18533df767bf2b3c9
vikas.kumar <krvikas1011@gmail.com>
2023-11-27 08:56:39 +0530

    added api template code

---
+++ enterprise-news-app-api/src/main/java/com/thisissvikas/enterprisenewsapp/EnterpriseNewsAppApplication.java
@@ -0,0 +1,13 @@
+package com.thisissvikas.enterprisenewsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseNewsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnterpriseNewsAppApplication.class, args);
+	}
+
+}
---
+++ enterprise-news-app-api/src/test/java/com/thisissvikas/enterprisenewsapp/EnterpriseNewsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thisissvikas.enterprisenewsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterpriseNewsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
