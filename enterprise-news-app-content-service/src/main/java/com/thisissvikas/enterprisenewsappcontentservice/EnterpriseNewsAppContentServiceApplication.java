318543fbf23b8e0a8e78d14217f0bef416a17924
Vikas Kumar <krvikas1011@gmail.com>
2023-12-03 16:08:41 +0530

    added content service skeleton

---
+++ enterprise-news-app-content-service/src/main/java/com/thisissvikas/enterprisenewsappcontentservice/EnterpriseNewsAppContentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.thisissvikas.enterprisenewsappcontentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseNewsAppContentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnterpriseNewsAppContentServiceApplication.class, args);
+	}
+
+}
---
+++ enterprise-news-app-content-service/src/test/java/com/thisissvikas/enterprisenewsappcontentservice/EnterpriseNewsAppContentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thisissvikas.enterprisenewsappcontentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterpriseNewsAppContentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
